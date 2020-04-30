cfeefc08e02e7514b75a051eedecb912272d04fd
rhavy.maia <rmg3@cin.ufpe.br>
2020-04-29 22:29:42 -0300

    Ajuste na listagem de contatos e empresas; Atualização dos dados da empresa pela app web.

--- ServicoApp/models/contato.py
+++ ServicoApp/models/contato.py
@@ -1,6 +1,7 @@
 from common.database import db
 from flask_restful import fields
 from sqlalchemy.ext.orderinglist import ordering_list
+from sqlalchemy.sql import func
 
 
 contato_campos = {
@@ -8,7 +9,8 @@ contato_campos = {
     'nome': fields.String(attribute='nome'),
     'email': fields.String(attribute='email'),
     'telefone': fields.String(attribute='telefone'),
-    'descricao': fields.String(attribute='descricao')
+    'descricao': fields.String(attribute='descricao'),
+    'isAtendido': fields.Boolean(attribute='is_atendido')
 }
 
 
@@ -23,12 +25,16 @@ class ContatoModel(db.Model):
     email = db.Column(db.String(255))
     telefone = db.Column(db.String(13))
     descricao = db.Column(db.Text())
+    is_atendido = db.Column(db.Boolean, default=False)
+    dt_insercao = db.Column(db.DateTime, default=func.current_timestamp())
     
-    def __init__(self, nome, email, telefone, descricao):
+    
+    def __init__(self, nome, email, telefone, descricao, is_atendido):
         self.nome = nome
         self.email = email
         self.telefone = telefone
         self.descricao = descricao
+        self.is_atendido = is_atendido
 
     def __str__(self):
         return '<Contato %r>'%(self.nome)
