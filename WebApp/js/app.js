31da9f330b10470df342e5ba6c6dfad172996c99
rhavymaia <rmg3@cin.ufpe.br>
2020-06-11 14:33:26 -0300

    Inclusão de campo select com busca textual na listagem das empresas para visitante.

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -2,5 +2,5 @@
 var nomeApp = 'LaNoCentro';
 var modulos = ['ngMessages', 'ngRoute', 'ngAnimate', 'ngAria', 'ngMaterial',
 'ui.router', 'md.data.table', 'ngMdIcons', 'ui.bootstrap', 'ui-leaflet',
-'ui.utils.masks', 'idf.br-filters'];
+'ui.utils.masks', 'idf.br-filters', 'ui.select', 'ngSanitize'];
 var app = angular.module(nomeApp, modulos);
